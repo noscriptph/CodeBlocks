32969aaea77d9af95df234d26dd66a5e69ab7f1c
victor <103396791+noscriptph@users.noreply.github.com>
2024-03-31 20:23:28 -0300

    se agrega catch and try

---
+++ java/catch and try.java
@@ -0,0 +1,31 @@
+/*debes importar la siguiente linea
+import java.util.Scanner;
+esta linea va arriba de todo en el archivo en el que estas programando,
+normalmente se usa en archivos de tipo clase
+*/
+
+//lo siguiente va dentro del metodo en el que estas trabajando, o bien si estas recien aprendiendo
+//a programar, va dentro de 
+//public class Main {
+//*aqui *
+//
+// que seria el metodo automaticamente creado en tu IDE
+
+boolean seguir = true;
+Scanner scanner = new Scanner(System.in);
+
+
+while (seguir){
+
+    try {
+
+        //ingresa tu codigo aqui para que se ejecute evitando errores al ingresar opciones en el
+
+
+    } catch (Exception e) {
+        throw new RuntimeException(e);
+      
+
+    }  seguir = false;
+
+}
